0dcdd97c6cc430a7ac80c57ff6da49171f0f0da0
Benane86 <47816453+Benane86@users.noreply.github.com>
2019-06-07 14:05:30 +0200

    Update Program.cs

--- ConsoleApp14/Program.cs
+++ ConsoleApp14/Program.cs
@@ -1,8 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+using System.Media;
+
 
 namespace ConsoleApp14
 {
@@ -10,6 +13,13 @@ namespace ConsoleApp14
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Time Einstellen, bitte gib eine Zeit in Sekunden an!");
+            int time1 = Convert.ToInt32(Console.ReadLine());
+            int sekunden = time1 * 1000;
+            Thread.Sleep(sekunden);
+            Console.WriteLine("es sind {0} sekunden vergangen", time1);
+            SystemSounds.Beep.Play();
+            Console.ReadKey();
         }
     }
 }
